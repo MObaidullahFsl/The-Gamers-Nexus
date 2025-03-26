9702884f54dc01193c6868c8768306337667d581
CyberSmeagol <mobaidullahfsl@gmail.com>
2025-03-26 06:25:42 +0500

    added Hashing

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import connectToDatabase from './db.js'; 
-
+import bcrypt from 'bcrypt'
 
 const app = express();
 const PORT = 5000; // ull see this in the url used to access backend
@@ -56,25 +56,31 @@ try {
 // all login logic, mw goes here
 app.post('/login',async (req,res)=>{
 try {
-  const pool = await connectToDatabase();
+
   const user = {name:req.body.name, password: req.body.password, email: req.body.email}
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(user.password,salt);
+  console.log(salt, hashedPassword);
+  const pool = await connectToDatabase();
   // ( 10, ${user.name}, ${user.password}, ${user.email} );
   console.log(user);
   const result = await pool.query(
     `insert into Users (username,password,email) values
-    ( '${user.name}', '${user.password}', '${user.email}');
+    ( '${user.name}', '${hashedPassword}', '${user.email}');
     `);
 
     if(result){
       res.status(201).send("Entered values successfully");
     }else{
-      res.status(400).send("Entered values failed");
+      res.status(400).send("Entered values failed",error.message);
+
     }
 } catch (error) {
-  console.log("Error inputting",error);
+  console.log("Error inputting",error, error.message);
   res.status(418).send("didnt work");
 }
 })
+
 app.all('/secret',(res,req)=>{
   console.log("Accessing the Secret Page...");
 })

d53cb761d4c3aba6caa3b0a620437dcd75bb098b
CyberSmeagol <mobaidullahfsl@gmail.com>
2025-04-02 16:42:38 +0500

    made auth routes

---
+++ src/assets/images/constants.js
@@ -0,0 +1,19 @@
+
+import logoDay from "./icons/logoDay.png"
+import login from "./icons/login.png"
+import create from "./icons/create.png"
+import google from "./icons/google.png"
+import discord from "./icons/discord.png"
+import whatsapp from "./icons/whatsapp.png"
+import instagram from "./icons/instagram.png"
+import arrow from "./icons/arrow.png"
+
+
+import th1 from "./thumbnails/cyberpunk.jpg"
+import th2 from "./thumbnails/isometric.jpeg"
+import th3 from "./thumbnails/miside.jpg"
+import th4 from "./thumbnails/acshadows.jpg"
+
+
+
+export default {  logoDay,login,create,google,discord,whatsapp,instagram,arrow, th1,th2,th3,th4 };
--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,45 +1,86 @@
-import React from 'react'
-import './SignIn.css'
+import React from "react";
+import "./SignIn.css";
+import images from "../../assets/images/constants";
 const SignIn = () => {
-
-
   return (
-    <div className='main'>
-        
-        <div className="leftSide">
-                Left
-            {/* <div className="logo"></div>
-            <div className="buttons"></div>
+    <div className="main">
+    
+      <div className="leftSide">
+    
+        <div className="logo">
+    
+          <img src={images.logoDay} alt="Logo" />
+    
+        </div>
+    
+        <div className="buttons">
+    
+          <div className="login">
+    
+            <img src={images.login}  alt="login" />
+    
+            <div>Log In</div>
+    
+          </div>
+    
+          <div className="createAccount">
+    
+            <img src={images.create} alt="create" />
+    
+            <div>Create Account</div>
+    
+          </div>
+    
+        </div>
 
-            <div className="greeting"></div>
+        <div className="greeting">
 
+            <div className="title">
+            Welcome back!<span> User</span>
+            </div>
+            <div className="msg">Get into your dashboard</div>
+        </div>
+        <div className="forgot">Forgot Password?</div>
+        <div className="continue"> 
+            <div className="text">
+            <div className="continueText">or Continue With </div> <div className="line"></div>
+            </div>
+          
+           
+
+        </div>
+
+        <div className="icons">
+
+<div > <img className="google" src={images.google} alt="google" /></div>
+<div >  <img className="discord" src={images.discord} alt="google" /></div>
+<div > <img className="instagram" src={images.instagram} alt="google" /></div>
+<div >  <img className="whatsapp" src={images.whatsapp} alt="google" /></div>
+
+</div>
+        {/* 
             <div className="email"></div>
 
             <div className="password"></div>
 
             <div className="send"></div>
 
-            <div className="continue">
+           */}
+      </div>
+      <div className="rightSide">
+            <div className="arrows">
+                
+              <div className="left"> <img src={images.arrow} alt="arrow left" /></div>
+              <div className="right"> <img src={images.arrow} alt="arrow left" /></div>
+            </div>
 
-                <div className="text"></div>
-                <div className="icons">
+      </div>
 
-                    <div className="google"></div>
-                    <div className="discord"></div>
-                    <div className="instagram"></div>
-                    <div className="reddit"></div>
+           
 
-                </div>
-
-            </div> */}
-
-        </div>
-        <div className="rightSide">
-                Right
-        </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;

601eadcd74641582af2bff08eb3b26146bfdc3e6
CyberSmeagol <mobaidullahfsl@gmail.com>
2025-04-03 21:06:55 +0500

    structured a bit

--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -1,5 +1,14 @@
+import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+      <>
+
+      
+      </>
+    );
   };
   
   export default Home;
